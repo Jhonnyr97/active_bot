2d9cfed6ec0ed7e07f155633342bf0b50bebcfcd
Nilthon Jhon Rojas Apumayta <oneill.jhon97@gmail.com>
2024-01-14 02:53:30 +0000

    first

---
+++ lib/active_bot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "active_bot/version"
+
+module ActiveBot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_bot/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveBot
+  VERSION = "0.1.0"
+end
